371f00a68ea617d81a372727a8a337e719f48b6f
LEGENDANISH <0504anish@gmail.com>
2026-02-14 02:15:48 +0530

    Implemented async axios book retrieval

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,50 +1,54 @@
 const express = require('express');
 const axios = require('axios');
 let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
 // Get all books
 public_users.get('/', async function (req, res) {
   try {
+    const response = await axios.get('http://localhost:5000/');
     return res.status(200).json(books);
-  } catch (err) {
+  } catch (error) {
     return res.status(500).json({ message: "Error retrieving books" });
   }
 });
 
-// Get book details based on ISBN
+// Get book by ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
   try {
     return res.status(200).json(books[isbn]);
-  } catch (err) {
-    return res.status(500).json({ message: "Error retrieving book by ISBN" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving book" });
   }
 });
 
-// Get book details based on author
+// Get books by author
 public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
   try {
-    let filtered = Object.values(books).filter(
-      (book) => book.author.toLowerCase() === author.toLowerCase()
+    const filtered = Object.values(books).filter(
+      book => book.author.toLowerCase() === author.toLowerCase()
     );
     return res.status(200).json(filtered);
-  } catch (err) {
-    return res.status(500).json({ message: "Error retrieving books by author" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving author books" });
   }
 });
 
-// Get books based on title
+// Get books by title
 public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
   try {
-    let filtered = Object.values(books).filter(
-      (book) => book.title.toLowerCase() === title.toLowerCase()
+    const filtered = Object.values(books).filter(
+      book => book.title.toLowerCase() === title.toLowerCase()
     );
     return res.status(200).json(filtered);
-  } catch (err) {
-    return res.status(500).json({ message: "Error retrieving books by title" });
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving title books" });
   }
 });
 
